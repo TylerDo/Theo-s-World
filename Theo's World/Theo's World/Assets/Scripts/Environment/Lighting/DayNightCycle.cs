a29aebc5d9c7fd718bf1d52c2529b50788c00f2a
Tyler Do <tdo2018@fau.edu>
2023-07-22 19:15:48 -0400

    Update DayNightCycle.cs
    Add min/max angles for light.
    Add durations for day/night

--- Theo's World/Theo's World/Assets/Scripts/Environment/Lighting/DayNightCycle.cs
+++ Theo's World/Theo's World/Assets/Scripts/Environment/Lighting/DayNightCycle.cs
@@ -7,29 +7,42 @@ public class DayNightCycle : MonoBehaviour
   public Light Sun;
   public Light Moon;
   public float lengthOfDay = 60f;
+  public float lengthOfNight = 60f;
   public float timeScale = 1f;
   private float timeOfDay = 0f;
+  public float minAngle = 60f; // Minimum angle of sun/moon movement
+  public float maxAngle = 120f; // Maximum angle of sun/moon movement
 
   void Update()
   {
     timeOfDay += Time.deltaTime * timeScale;
-    if (timeOfDay > lengthOfDay)
+    if (timeOfDay > lengthOfDay + lengthOfNight)
     {
-      timeOfDay -= lengthOfDay;
+      timeOfDay -= lengthOfDay + lengthOfNight;
     }
-    float sunAngle = (timeOfDay / lengthOfDay) * 360f;
-    Sun.transform.rotation = Quaternion.Euler(sunAngle, 0, 0);
-    float moonAngle = sunAngle - 180f;
-    Moon.transform.rotation = Quaternion.Euler(moonAngle, 0, 0);
-    if (sunAngle > 90f && sunAngle < 270f)
+    // Calculate the angle of the sun and moon based on the current time of day, the minimum and maximum angles, and the length of day and night
+    float sunAngle, moonAngle;
+    if (timeOfDay < lengthOfDay)
     {
-      Sun.enabled = false;
-      Moon.enabled = true;
+      sunAngle = Mathf.Lerp(minAngle, maxAngle, timeOfDay / lengthOfDay);
+      Sun.transform.rotation = Quaternion.Euler(sunAngle, 0, 0);
     }
     else
     {
+      //sunAngle = Mathf.Lerp(maxAngle, minAngle, (timeOfDay - lengthOfDay) / lengthOfNight);
+      moonAngle = Mathf.Lerp(minAngle, maxAngle, (timeOfDay - lengthOfDay) / lengthOfNight);
+      Moon.transform.rotation = Quaternion.Euler(moonAngle, 0, 0);
+    }
+    // Rotate the sun and moon to the calculated angles
+    if (timeOfDay < lengthOfDay)
+    {
       Sun.enabled = true;
       Moon.enabled = false;
+    }
+    else
+    {
+      Sun.enabled = false;
+      Moon.enabled = true;
     }
   }
 }
