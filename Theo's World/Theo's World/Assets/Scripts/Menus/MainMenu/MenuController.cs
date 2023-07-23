f8fab9314b86d5822eb59c8c61ca627284560130
Tyler Do <tdo2018@fau.edu>
2023-07-23 17:01:37 -0400

    Update MenuController.cs
    Add navigation with dpad and horizontal direction.
    Add references to multiple menus.
    Add ability to go back to main menu.
    Add events to navigate between menus.

--- Theo's World/Theo's World/Assets/Scripts/Menus/MainMenu/MenuController.cs
+++ Theo's World/Theo's World/Assets/Scripts/Menus/MainMenu/MenuController.cs
@@ -2,20 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Composites;
 using UnityEngine.InputSystem.DualShock;
 using UnityEngine.UI;
 
 public class MenuController : MonoBehaviour
 {
-  public GameObject menu;
+  public GameObject newGameMenu;
+  public GameObject loadGameMenu;
+  public GameObject optionsMenu;
+  public GameObject settingsMenu;
+  public GameObject mainMenu;
   public Collection<Button> menuButtons = new Collection<Button>();
+  public Collection<Canvas> menus;
   private DualShock4GamepadHID PS4Controller;
   private int selectedIndex = 0;
   public float navigationThreshold = 0.5f; // The threshold for navigating between buttons
   private bool canNavigate = true; // Whether or not navigation is currently allowed
   private float lastInputTime = 0f; // The time of the last input event
   public float inputDelay = 0.2f; // The delay between input events in seconds
+  private bool isVerticalButtonLayout = true;
   void Start()
   {
     foreach (Gamepad gp in Gamepad.all)
@@ -26,8 +34,8 @@ public class MenuController : MonoBehaviour
       }
     }
     // Find all Button components that are children of the menu game object
-    menuButtons = new Collection<Button>(menu.GetComponentsInChildren<Button>());
-    foreach(Button b in menuButtons)
+    menuButtons = new Collection<Button>(mainMenu.GetComponentsInChildren<Button>());
+    foreach (Button b in menuButtons)
     {
       Debug.Log(b);
     }
@@ -35,9 +43,9 @@ public class MenuController : MonoBehaviour
     SelectButton(0);
   }
 
-    // Update is called once per frame
-    void Update()
-    {
+  // Update is called once per frame
+  void Update()
+  {
     // Check if enough time has passed since the last input event
     if (Time.time - lastInputTime > inputDelay)
     {
@@ -47,31 +55,11 @@ public class MenuController : MonoBehaviour
         // Check if navigation is allowed
         if (canNavigate)
         {
-          if (PS4Controller.leftStick.down.isPressed)
-          {
-            Debug.Log("Down");
-            if (selectedIndex < menuButtons.Count - 1)
-            {
-              SelectButton(selectedIndex + 1);
-              canNavigate = false;
-              lastInputTime = Time.time;
-            }
-          }
-          else if (PS4Controller.leftStick.up.isPressed)
-          {
-            Debug.Log("Up");
-
-            if (selectedIndex > 0)
-            {
-              SelectButton(selectedIndex - 1);
-              canNavigate = false;
-              lastInputTime = Time.time;
-            }
-          }
+          ButtonNavigation();
         }
         else
         {
-          if(PS4Controller.leftStick.ReadValue() == Vector2.zero)
+          if (PS4Controller.leftStick.ReadValue() == Vector2.zero && PS4Controller.dpad.ReadValue() == Vector2.zero)
           {
             canNavigate = true;
           }
@@ -83,6 +71,62 @@ public class MenuController : MonoBehaviour
           Debug.Log("Selecting button " + selectedIndex.ToString());
           // Activate the currently selected button
           menuButtons[selectedIndex].onClick.Invoke();
+        }
+        if (PS4Controller.circleButton.isPressed)
+        {
+          GoBacktoMainMenu();
+        }
+      }
+    }
+  }
+
+  private void ButtonNavigation()
+  {
+    if (isVerticalButtonLayout)
+    {
+      if (PS4Controller.leftStick.down.isPressed || PS4Controller.dpad.down.isPressed)
+      {
+        Debug.Log("Down");
+        if (selectedIndex < menuButtons.Count - 1)
+        {
+          SelectButton(selectedIndex + 1);
+          canNavigate = false;
+          lastInputTime = Time.time;
+        }
+      }
+      else if (PS4Controller.leftStick.up.isPressed || PS4Controller.dpad.up.isPressed)
+      {
+        Debug.Log("Up");
+
+        if (selectedIndex > 0)
+        {
+          SelectButton(selectedIndex - 1);
+          canNavigate = false;
+          lastInputTime = Time.time;
+        }
+      }
+    }
+    else
+    {
+      if (PS4Controller.leftStick.right.isPressed || PS4Controller.dpad.right.isPressed)
+      {
+        Debug.Log("Down");
+        if (selectedIndex < menuButtons.Count - 1)
+        {
+          SelectButton(selectedIndex + 1);
+          canNavigate = false;
+          lastInputTime = Time.time;
+        }
+      }
+      else if (PS4Controller.leftStick.left.isPressed || PS4Controller.dpad.left.isPressed)
+      {
+        Debug.Log("Up");
+
+        if (selectedIndex > 0)
+        {
+          SelectButton(selectedIndex - 1);
+          canNavigate = false;
+          lastInputTime = Time.time;
         }
       }
     }
@@ -102,5 +146,55 @@ public class MenuController : MonoBehaviour
     selectedButton.Select();
     selectedButton.OnSelect(null);
     selectedIndex = index;
+  }
+
+  private void GoBacktoMainMenu()
+  {
+    Debug.Log("GobackToMainMenu");
+    if(mainMenu is not null)
+    {
+      mainMenu.SetActive(true);
+    }
+    if (newGameMenu is not null)
+    {
+      newGameMenu.SetActive(false);
+    }
+    if (loadGameMenu is not null)
+    {
+      loadGameMenu.SetActive(false);
+    }
+    if (optionsMenu is not null)
+    {
+      optionsMenu.SetActive(false);
+    }
+    if (settingsMenu is not null)
+    {
+      settingsMenu.SetActive(false);
+    }
+    menuButtons = new Collection<Button>(mainMenu.GetComponentsInChildren<Button>());
+  }
+   
+  public void UpdateButtonCollection(GameObject button)
+  {
+    menuButtons = button.name switch
+    {
+      "NewGameButton" => new Collection<Button>(newGameMenu.GetComponentsInChildren<Button>()),
+      "LoadGameButton" => new Collection<Button>(loadGameMenu.GetComponentsInChildren<Button>()),
+      "OptionsButton" => new Collection<Button>(optionsMenu.GetComponentsInChildren<Button>()),
+      "SettingsButton" => new Collection<Button>(settingsMenu.GetComponentsInChildren<Button>()),
+      _ => new Collection<Button>(mainMenu.GetComponentsInChildren<Button>()),
+    };
+    isVerticalButtonLayout = button.name switch
+    {
+      "NewGameButton" => true,
+      "LoadGameButton" => true,
+      "OptionsButton" => false,
+      "SettingsButton" => false,
+      _ => true,
+    };
+    foreach (Button b in menuButtons)
+    {
+      Debug.Log(b);
+    }
   }
 }
